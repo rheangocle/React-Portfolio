a58428d01b83bff76b7da36a2b29138fd393bd1e
Rhea Le <106790436+rheangocle@users.noreply.github.com>
2022-08-22 11:28:35 -0400

    edited margins and colors for projects

--- my-app/src/components/Projects.js
+++ my-app/src/components/Projects.js
@@ -30,12 +30,14 @@ export default function Projects() {
                   className='py-5 absolute w-full h-full max-h-72 object-cover object-left-top'
                   src={project.image}
                 />
-                <div className='px-10 py-20 mb-10 relative z-10 w-full border-4 border-stone-500 bg-slate-900 opacity-0 hover:opacity-100'>
+                <div className='px-10 py-20 mb-10 relative z-10 w-full border-4 border-stone-300 bg-slate-900 opacity-0 hover:opacity-100'>
                   <h1 className='title-font text-lg font-medium text-white mb-3'>
                     {project.title}
                   </h1>
-                  <p className='leading-relaxed'>{project.description}</p>
-                  <h2 className='tracking-widest text-sm title-font font-medium text-green-400 my-1'>
+                  <p className='leading-relaxed text-stone-400 p-1'>
+                    {project.description}
+                  </p>
+                  <h2 className='tracking-widest text-sm title-font font-medium text-lime-300 my-1'>
                     Technologies: {project.subtitle}
                   </h2>
                 </div>
